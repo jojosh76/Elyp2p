0e7de38435c27cd6fe52e1e1d6625dcd4fb34e7e
jojosh76 <j7786196@gmail.com>
2026-06-25 09:56:39 +0100

    Change commission and travelerAmount to constants

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -19,8 +19,8 @@ void main() {
     test('Commission rate calculation', () {
       const amount = 100.0;
       const rate = 0.10;
-      final commission = amount * rate;
-      final travelerAmount = amount - commission;
+      const commission = amount * rate;
+      const travelerAmount = amount - commission;
       expect(commission, equals(10.0));
       expect(travelerAmount, equals(90.0));
     });
